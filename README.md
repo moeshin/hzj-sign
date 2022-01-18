569be69e7ed5b7fa0142076b1a12d68b17bac26c
MoeShin <hi@moeshin.com>
2022-01-19 04:21:09 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # hzj-sign
+
 慧职教签到
+
+## 配置文件
+
+创建或编辑 `config.json` 文件
+
+| 字段       | 说明                        |
+|----------|---------------------------|
+| token    | 账号凭证                      |
+| trip     | 是否出差，默认 `true`            |
+| address  | 可空，中文地址                   |
+| location | 可空，经纬度                    |
+| pushplus | 可空，[PushPlus] 消息推送的 Token |
+
+### `token` 账号凭证
+
+登录 http://gzdk.gzisp.net/hzjmui/mui/login.html 后，  
+按 `F12` 调出 `开发者工具`，查看 `应用` - `会话存储空间` 里的 `token` 值。  
+或在 `控制台` 中执行 `sessionStorage.token` 查看。
+
+### `location` 经纬度
+
+如：`116.422522-39.901873`，可通过 https://tool.lu/coordinate/ 获取。
+
+[PushPlus]: https://www.pushplus.plus/
---
+++ sign.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import http.client
+import json
+import os
+import urllib.parse
+
+
+def sign(token, address='', location='', trip=False):
+    conn = http.client.HTTPConnection('gzdk.gzisp.net', 8082)
+    conn.request('POST', '/sign/addSignIn?' + urllib.parse.urlencode({
+        'SCALE': 18,
+        'authorization': token,
+        'ISEVECTION': int(trip),
+        'ADDR': address,
+        'AXIS': location,
+
+        # 以下参数可有可无
+        'CONTENT': '',
+        'IDS': '',
+        'token': token,
+    }))
+    res = conn.getresponse()
+    body = res.read()
+    try:
+        if json.loads(body).get('res') == 'success':
+            return
+    except json.decoder.JSONDecodeError:
+        pass
+    code = res.getcode()
+    try:
+        status = http.HTTPStatus(code).phrase
+    except ValueError:
+        status = ''
+    raise Exception('%d %s\n%s' % (code, status, str(body, 'utf-8')))
+
+
+def push_plus_notify(token, title, content, template='html'):
+    args = json.dumps(locals())
+    conn = http.client.HTTPSConnection('www.pushplus.plus')
+    conn.request('POST', '/send/', args)
+    res = conn.getresponse()
+    body = res.read()
+    try:
+        if json.loads(body).get('code') == 200:
+            return
+    except json.decoder.JSONDecodeError:
+        pass
+    code = res.getcode()
+    try:
+        status = http.HTTPStatus(code).phrase
+    except ValueError:
+        status = ''
+    raise Exception('%d %s\n%s' % (code, status, str(body, 'utf-8')))
+
+
+def main():
+    config_path = os.path.dirname(os.path.abspath(__file__)) + '/config.json'
+    try:
+        with open(config_path, encoding='utf-8') as fp:
+            config = json.load(fp)
+    except FileNotFoundError:
+        print("配置文件不存在：%s" % config_path)
+        exit(1)
+
+    def notify(msg):
+        print(msg)
+        token = config.get('pushplus')
+        if token is not None:
+            push_plus_notify(token, '慧职教签到', '```text\n' + msg + '\n```', 'markdown')
+
+    try:
+        sign(config['token'], config.get('address', ''), config.get('location', ''), config.get('trip', False))
+        notify('签到成功！')
+    except Exception as e:
+        notify('签到失败：' + str(e))
+
+
+if __name__ == '__main__':
+    main()
